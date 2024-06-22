05c7abd45ac44215a43cf5bc56f6408ec1a5d87b
Affnan makandar <123863231+Affnanmakandar@users.noreply.github.com>
2024-06-22 13:06:18 +0530

    A simple assignment
    Created a simple layout of three grid cards using bootstrap html and used css to give hover effect

---
+++ project-bootstrap/itech-card.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Document</title>
+</head>
+<body>
+   <div class="container">
+       <div class="row">
+           <div class="col-lg-12">
+               <h1 class="text-center mt-4 col-md-12">Professional Courses</h1>
+               <p class="text-center"><span class="text-danger">Elevate </span>Your Expertise: Master Professional Computer Courses for Tomorrow's Tech World</p>
+           </div>
+           <div class="col-lg-3 border border-info data p-1 mt-4 mb-4 mx-4  ">
+               <p class="text-center mt-2">Data Analytics</p>
+           </div>
+           <div class="col-lg-3 border border-info animation p-1 mt-4 mb-4 mx-4 ">
+               <p class="text-center mt-2">Animation</p>
+           </div>
+           <div class="col-lg-3 border border-info software p-1 mt-4 mb-4 mx-4  ">
+               <p class="text-center mt-2">Software Engineering</p>
+           </div>
+           <div class="col-lg-3 border border-info data p-1 mt-4 mb-4 mx-4">
+               <p class="text-center mt-2">Full stack Development</p>
+           </div>
+           <div class="col-lg-3 border border-info animation p-1 mt-4 mb-4 mx-4">
+               <p class="text-center mt-2">Data Science</p>
+           </div>
+           <div class="col-lg-3 border border-info software p-1 mt-4 mb-4 mx-4">
+               <p class="text-center mt-2">Digital Marketing</p>
+           </div>
+           <div class="col-lg-3 border border-info data p-1 mt-4 mb-4 mx-4">
+               <p class="text-center mt-2">Businessfinance & Tally</p>
+           </div>
+           <div class="col-lg-3 border border-info animation p-1 mt-4 mb-4 mx-4">
+               <p class="text-center mt-2">Animation</p>
+           </div>
+           <div class="col-lg-3 border border-info software p-1 mt-4 mb-4 mx-4">
+               <p class="text-center mt-2">Software Engineering</p>
+           </div>
+       </div>
+   </div>
+</body>
+</html>
